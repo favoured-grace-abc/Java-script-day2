0b33a21a521c90d13e3dde6c95ae6b5f74935fce
Nathan Grace <favouramaraci1@gmail.com>
2026-03-11 15:59:57 +0100

    feat: student-score

---
+++ student-score.js
@@ -0,0 +1,22 @@
+const score = [90, 80, 70, 60, 50]
+for (i of score){
+    if (i >= 90) {
+        console.log(`${i}: Grade-A`);
+    }
+    else if (i >= 80) {
+        console.log(`${i}: Grade-B`);
+    }
+    else if (i >= 70) {
+        console.log(`${i}: Grade-C`);
+    }
+    else if (i >= 60) {
+        console.log(`${i}: Grade-D`);
+    }
+    else {
+        console.log(`${i}: Grade-F`);
+    }
+
+}
+
+      
+    
