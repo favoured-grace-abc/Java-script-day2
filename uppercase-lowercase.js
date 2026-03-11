0820ee1d7f8fb42a290b8784398e389608834309
Nathan Grace <favouramaraci1@gmail.com>
2026-03-11 17:04:40 +0100

    feat: uppercase-lowercase

---
+++ uppercase-lowercase.js
@@ -0,0 +1,16 @@
+// Given a list of words, print each word in uppercase if it has more than 4 letters, lowercase if it has fewer, and title case if it has exactly 4.
+
+
+const names =["Amarachi", "John", "Grace", "Faith", "Joy", "Mary", "Peter", "James"];
+
+for(let i of names){
+    if(i.length > 4){
+        console.log(i.toUpperCase());
+    }
+    else if(i.length < 4){
+        console.log(i.toLowerCase());
+    }
+    else{
+      console.log(i.charAt(0).toUpperCase() + i.slice(1).toLowerCase());
+    }
+}
