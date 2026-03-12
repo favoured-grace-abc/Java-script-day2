8c6c14bbc377882683aa26d0fa51d3b2244b8734
Nathan Grace <favouramaraci1@gmail.com>
2026-03-12 13:44:33 +0100

    feat: uppercase-lowercase because corrections was made

--- uppercase-lowercase.js
+++ uppercase-lowercase.js
@@ -1,13 +1,19 @@
-const names =["Amarachi", "John", "Grace", "Faith", "Joy", "Mary", "Peter", "James"];
+let names = "Amarachi John Grace Faith Joy Mary Peter James";
+let currentName = "";
 
-for(let i of names){
-    if(i.length > 4){
-        console.log(i.toUpperCase());
-    }
-    else if(i.length < 4){
-        console.log(i.toLowerCase());
-    }
-    else{
-      console.log(i.charAt(0).toUpperCase() + i.slice(1).toLowerCase());
+for (let i = 0; i <= names.length; i++) {
+    if (i === names.length || names[i] === " ") {
+       
+        if (currentName.length > 4) {
+            console.log(currentName.toUpperCase());
+        } else if (currentName.length < 4) {
+            console.log(currentName.toLowerCase());
+        } else {
+            console.log(currentName[0].toUpperCase() + 
+                       currentName.slice(1).toLowerCase());
+        }
+        currentName = ""; 
+    } else {
+        currentName += names[i]; 
     }
 }
